a81a071763b512b75d665a397bc7676e0662c0d9
Gabriel Nützi (UBUNTU) <gnuetzi@gmx.ch>
2015-02-16 19:51:11 +0100

    changed stupid chacnge to std:.vector< value_type > in ContactGraph -> reallocation makes dangling pointers!

--- SourceCode/Projects/SimulationFramework/CommonSource/src/GRSF/Dynamics/Inclusion/ContactParameterMap.cpp
+++ SourceCode/Projects/SimulationFramework/CommonSource/src/GRSF/Dynamics/Inclusion/ContactParameterMap.cpp
@@ -8,32 +8,11 @@ ContactParameterTag::ContactParameterTag( unsigned int materialid1, unsigned int
     if(materialid1>materialid2) {
         std::swap(materialid1,materialid2);
     }
-    m_tag = boost::make_tuple(materialid1,materialid2);
+    m_tag = std::make_tuple(materialid1,materialid2);
 }
 
 bool ContactParameterTag::operator==(ContactParameterTag const& c2) const {
-    if( (   m_tag.get<0>() == c2.m_tag.get<0>() &&
-            m_tag.get<1>() == c2.m_tag.get<1>()
-         )
-        || ( //or if it is switched
-            m_tag.get<0>() == c2.m_tag.get<1>() &&
-            m_tag.get<1>() == c2.m_tag.get<0>()
-        )
-      )
-    {
-        return true;
-    }
-    return false;
-}
-
-
-std::size_t ContactParameterTagHash::operator()(ContactParameterTag const& c) const {
-    std::size_t seed = 0;
-
-    boost::hash_combine(seed, c.m_tag.get<0>());
-    boost::hash_combine(seed, c.m_tag.get<1>());
-
-    return seed;
+   return m_tag == c2.m_tag;
 }
 
 
