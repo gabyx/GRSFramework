4bc7cb7c7e1ab2931361d3ce5eb825bcfc45461b
Gabriel Nützi (UBUNTU) <gnuetzi@gmx.ch>
2015-02-24 19:56:23 +0100

    finished contact graph design

--- SourceCode/Projects/SimulationFramework/CommonSource/include/GRSF/Common/TupleHelper.hpp
+++ SourceCode/Projects/SimulationFramework/CommonSource/include/GRSF/Common/TupleHelper.hpp
@@ -50,5 +50,34 @@ struct TupleIndex<T, std::tuple<U, Types...> > {
     static const std::size_t value = 1 + TupleIndex<T, std::tuple<Types...> >::value;
 };
 
+
+
+
+/**  Moves visitor over all entries */
+struct TupleVisit{
+
+    template<typename TupleType,
+             unsigned int Idx = std::tuple_size<TupleType>::value -1 ,
+             typename Visitor,
+             typename std::enable_if< (Idx > 0), void*>::type = nullptr
+                     >
+    static void dispatch(Visitor & v, TupleType & t) {
+        auto & c = std::get<Idx>(t);
+        v(c);
+        TupleVisit::dispatch<TupleType,Idx-1>(v,t);
+    }
+
+    template<typename TupleType,
+             unsigned int Idx,
+             typename Visitor,
+             typename std::enable_if< Idx == 0, void*>::type = nullptr
+             >
+    static void dispatch(Visitor & v, TupleType & t) {
+        auto & c = std::get<Idx>(t);
+        v(c);
+    }
+};
+
+
 #endif
 
