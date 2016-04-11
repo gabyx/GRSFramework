257fe69ff1b660ec1556abe2aa7969167f781218
Gabriel Nützi (UBUNTU) <gnuetzi@gmx.ch>
2016-04-11 18:30:29 +0200

    all

--- MathJaxTest.md
+++ MathJaxTest.md
@@ -1,35 +1,10 @@
-The output of the simulator tools ``GRSFSim``, ``GRSFSimMPI``, ``GRSFGUI`` is a binary file [``.sim``](https://github.com/gabyx/GRSFramework/wiki/MultiBodySimFile) containing for each body a generalized displacement
-`\( \mathbf{q} =  [{_\mathrm{I}}\mathbf{r}_{S}^\top \ , \ \mathbf{p}_{\mathrm{KI}}^\top]^\top \in \mathbb{R}^7 \)` and a generalized velocity `\( \mathbf{u} =  [{_\mathrm{I}}\dot{\mathbf{r}}_{S}^\top \ , \ {_\mathrm{K}}\boldsymbol{\Omega}^\top]^\top \in \mathbb{R}^6 \)`. 
+
+\\( \mathbf{q} =  [{\_\mathrm{I}}\mathbf{r}\_{S}^\top \ , \ \mathbf{p}\_{\mathrm{KI}}^\top]^\top \in \mathbb{R}^7 \\) 
+
+$$asd$$
 
 
-# Head 1
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-## Chapter 1.1
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-## Chapter 1.2
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-
-# Head 2
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-## Chapter 2.1
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-### Title 2.1.1
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-### Title 2.1.2
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-### Title 2.1.3
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-## Chapter 2.2
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-### Title 2.2.1
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-### Title 2.2.2
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-### Title 2.2.3
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-
-# Head 3
-## Chapter 3.1
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
-## Chapter 3.2
-asjd ljasldjasldj ljf lakjdsf asdf asdf asdf alskdjf ölakjsdflk jasdlkfj alösdfj laskdjf lkajsdfl jasdlfk jaslödfjk löaskdjf lökajsdlfö kjasldöfkj alöskdjf löakjsdlöf kjasdlöfjk alösdkjf laskjdf lökajsd lfö kjaslödfj alösdkjf löakjsdfl kjasdlfkj alsdkjf laskdjf lkaj
+```
+    <H1>HTML code</H1>
+    <p class="some">This is an example</p>
+```
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,42 @@
     
-    var markdownBuffer = document.createElement("div")
+    var mkConverter = new showdown.Converter(
+    { literalMidWordUnderscores : true,
+      tables : true,
+      ghCodeBlocks : true,
+      tasklists : true}
+    );
+    
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: false, // IMPORTANT, because we do MathJax before markdown,
+                       //            however we do escaping in 'CreatePreview'.
+      smartLists: true,
+      smartypants: false,
+      highlight: function(code) {
+        return hljs.highlightAuto(code).value;
+      }
+    });
+    
+    // use marked
+    var renderMKtoHTML = function (text){ return marked(text); };
+    
+    // use showdown
+    //var renderMKtoHTML = function (text){ return mkConverter.makeHtml(text); };
+        
+    var markdownBufferText = null;
+    var markdownBufferDiv = document.createElement("div");
+    var $markdownBufferDiv = null;
     
     function renderAll(){
       
       //MathJax.Hub.Startup.signal.Interest(function (message) {console.log("Startup: "+message)});
       //MathJax.Hub.signal.Interest(function (message) {console.log("Hub: "+message)});
 
-      // load wiki Home markup -> render mathjax -> render markdown -> replace videos -> parse/apply toc
+      // load wiki Home markup -> render markdown -> render mathjax -> replace videos -> parse/apply toc
       {
         //var fileUrl = "MathJaxTest.md" ; 
         var fileUrl = "https://rawgit.com/wiki/gabyx/GRSFramework/Home.md";
@@ -14,71 +44,56 @@
         
         console.log("get: ", fileUrl);
         client1.open('GET', fileUrl);
-        
+        //client1.overrideMimeType("text/plain; charset=utf-8");
         client1.onreadystatechange = function() {
             if(client1.readyState == 4){
                 console.log("loaded: ", fileUrl);
                 
                 // put loaded markdown text into content
                 //document.getElementById('content').innerHTML = client1.responseText;
-                markdownBuffer.innerHTML = client1.responseText;
                 
-                console.log("1. run mathjax");
-                MathJax.Hub.Typeset(markdownBuffer, renderMarkDown);
-                
+                console.log("render markdown");
+                markdownBufferDiv.innerHTML = renderMKtoHTML(client1.responseText);
+                $markdownBufferDiv = $(markdownBufferDiv);
+                               
+                // load video subtitles
+                jQuery.get('videoSubtitle.md',function(data){
+                  
+                  $markdownBufferDiv.find("#videoSubtitle").html(  renderMKtoHTML(data) );
+                  
+                  //replace videos with storage
+                  console.log("replace videos");
+                  $markdownBufferDiv.find("#videos-placeholder").replaceWith( $("#videoStorage") );
+                  
+                  // remove loader div
+                  $(".loaderdiv").fadeOut("slow", function (){
+                    
+                    // make content appear
+                    $("#content").html($markdownBufferDiv);
+                    
+                    console.log("3. parse/apply TOC");
+                    parseTOC("#content",'#toc-level1')
+                    applyTOC('#toc-button','#toc');
+                    
+                    var $t=$(window.location.hash);
+                    if($t[0]){
+                      $t[0].scrollIntoView(true);
+                    }
+                    
+                    console.log("run mathjax");
+                    MathJax.Hub.Typeset(document.getElementById('content'));
+                    
+                    
+                  });
+                  
+                });
+
             }
         }
         client1.send();
       }
     };
-    
-    function renderMarkDown(){
-        
 
-        
-        marked.setOptions({
-          renderer: new marked.Renderer(),
-          gfm: true,
-          tables: true,
-          breaks: false,
-          pedantic: false,
-          sanitize: false, // IMPORTANT, because we do MathJax before markdown,
-                           //            however we do escaping in 'CreatePreview'.
-          smartLists: true,
-          smartypants: false,
-          highlight: function(code) {
-            return hljs.highlightAuto(code).value;
-          }
-        });
-
-        // load video subtitles
-        jQuery.get('videoSubtitle.md',function(data){
-          $("#videoSubtitle").html(  marked(data) );
-        });
-        
-        var markedText = marked(markdownBuffer.innerHTML);
-        
-        // replace videos with storage
-        console.log("2. replace videos");
-        $(markedText).find("#videos-placeholder").replaceWith( $("#videoStorage") );
-        
-        // remove loader div
-        $(".loaderdiv").fadeOut("slow", function (){
-                // make content appear
-                document.getElementById('content').innerHTML = markedText;
-                
-                console.log("3. parse/apply TOC");
-                parseTOC("#content",'#toc-level1')
-                applyTOC('#toc-button','#toc');
-                
-                $(window.location.hash)[0].scrollIntoView(true);
-                
-            }
-        );
-        
-       
-    }
-    
     function parseTOC(from, to) {
       // parse in the TOC from marked markdown text
       var tocPlaceholder = $(to);
