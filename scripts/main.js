240ba6ffd96ffbbc9696299e0e45a1bee9b430b7
Gabriel Nützi <gnuetzi@gmx.ch>
2016-04-19 23:08:39 +0200

    alll

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-    
+
     var mkConverter = new showdown.Converter(
     { literalMidWordUnderscores : true,
       tables : true,
       ghCodeBlocks : true,
       tasklists : true}
     );
-    
+
     marked.setOptions({
       renderer: new marked.Renderer(),
       gfm: true,
@@ -20,72 +20,72 @@
         return hljs.highlightAuto(code).value;
       }
     });
-    
+
     // use marked
     var renderMKtoHTML = function (text){ return marked(text); };
-    
+
     // use showdown
     //var renderMKtoHTML = function (text){ return mkConverter.makeHtml(text); };
-        
+
     var markdownBufferText = null;
     var markdownBufferDiv = document.createElement("div");
     var $markdownBufferDiv = null;
-    
+
     function renderAll(){
-      
+
       //MathJax.Hub.Startup.signal.Interest(function (message) {console.log("Startup: "+message)});
       //MathJax.Hub.signal.Interest(function (message) {console.log("Hub: "+message)});
 
       // load wiki Home markup -> render markdown -> render mathjax -> replace videos -> parse/apply toc
       {
-        //var fileUrl = "MathJaxTest.md" ; 
+        //var fileUrl = "MathJaxTest.md" ;
         var fileUrl = "https://rawgit.com/wiki/gabyx/GRSFramework/Home.md";
         var client1 = new XMLHttpRequest();
-        
+
         console.log("get: ", fileUrl);
         client1.open('GET', fileUrl);
         //client1.overrideMimeType("text/plain; charset=utf-8");
         client1.onreadystatechange = function() {
             if(client1.readyState == 4){
                 console.log("loaded: ", fileUrl);
-                
+
                 // put loaded markdown text into content
                 //document.getElementById('content').innerHTML = client1.responseText;
-                
+
                 console.log("render markdown");
                 markdownBufferDiv.innerHTML = renderMKtoHTML(client1.responseText);
                 $markdownBufferDiv = $(markdownBufferDiv);
-                               
+
                 // load video subtitles
                 jQuery.get('videoSubtitle.md',function(data){
-                  
-                  $markdownBufferDiv.find("#videoSubtitle").html(  renderMKtoHTML(data) );
-                  
+
                   //replace videos with storage
                   console.log("replace videos");
                   $markdownBufferDiv.find("#videos-placeholder").replaceWith( $("#videoStorage") );
-                  
+
+                  $markdownBufferDiv.find("#videoSubtitle").html(  renderMKtoHTML(data) );
+
                   // remove loader div
                   $(".loaderdiv").fadeOut("slow", function (){
-                    
+
                     // make content appear
                     $("#content").html($markdownBufferDiv);
-                    
+
                     console.log("3. parse/apply TOC");
                     parseTOC("#content",'#toc-level1')
                     applyTOC('#toc-button','#toc');
-                    
+
                     var $t=$(window.location.hash);
                     if($t[0]){
                       $t[0].scrollIntoView(true);
                     }
-                    
+
                     console.log("run mathjax");
                     MathJax.Hub.Typeset(document.getElementById('content'));
-                    
-                    
+
+
                   });
-                  
+
                 });
 
             }
@@ -108,25 +108,25 @@
         prevTitle.not(":has(ul)").append('<ul></ul>');
         prevTitle.find('ul').append('<li id="'+ $(this).attr('id') + '-menu"><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
       });
-      
+
       staticContent.find('h3').each(function() {
         prevTitle = tocPlaceholder.find('#' + $(this).prevAll('h2').first().attr('id') + '-menu');
         prevTitle.not(":has(ul)").append('<ul></ul>');
         prevTitle.find('ul').append('<li id="'+ $(this).attr('id') + '-menu"><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
       });
-      
+
     }
 
     function applyTOC(buttonSel,srcSel) {
-      
+
       var set = false,
       $button = $(buttonSel).sidr({
         name: 'sidr-toc',
         source: srcSel
       });
-      
+
       var startOffset = 360;
-      
+
       $(window).scroll(function() {
         var top = window.scrollY;
         if (top > startOffset && !set) {
@@ -138,22 +138,21 @@
           set = false;
         }
       });
-      
+
       // add class 'nav' to  make scrollspy work
       $(".sidr-inner").addClass("nav");
-      
+
       // add expList class to make expandable
       $("#sidr-id-toc-level1").addClass("expList");
       prepareList("#sidr-id-toc-level1");
-      
+
       // add +/- button to sidr-toc
       $("#sidr-id-table-of-contents").prepend($("#toc-buttons-expcoll"));
-      
+
       $('body').scrollspy({offset:400});
-      
+
     };
-    
-    
-    // register renderAll to 
+
+
+    // register renderAll to
     $(window).load( renderAll );
-    
