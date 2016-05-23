a1e86c14855cda146799d843f16b3b2a2598ae13
Gabriel Nützi (UBUNTU) <gnuetzi@gmx.ch>
2016-05-23 16:26:46 +0200

    travis update

--- travis/install_dep.sh
+++ travis/install_dep.sh
@@ -42,7 +42,7 @@ wget --no-verbose --output-document="${ROOT_PATH}/boost.tar.bz2" "$BOOST_DOWNLOA
 cd ${BOOST_BUILD}
 tar jxf "${ROOT_PATH}/boost.tar.bz2" --strip-components=1 -C "${BOOST_BUILD}"
 ./bootstrap.sh --with-toolset="$BOOST_TOOLSET" --with-libraries=system,thread,serialization
-sudo ./b2 toolset="$BOOST_TOOLSET" threading=multi link=shared release install
+sudo ./b2 toolset="$BOOST_TOOLSET" threading=multi link=shared release install > /dev/null
 
 
 # Install OGRE 3d
