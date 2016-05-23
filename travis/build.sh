e3c959985f0a2c2ef3b42b18edeac53bc13a3d05
Gabriel Nützi (UBUNTU) <gnuetzi@gmx.ch>
2016-05-23 14:55:08 +0200

    added travis build

---
+++ travis/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# "BUILD ========================================================================"
+
+
+cd $ROOT_PATH
+if [ -z "$BUILD_TYPE" ]; then export BUILD_TYPE=Release; fi
+
+
+
+echo "Build GRSFramework:"
+cd $CHECKOUT_PATH
+
+### init submodules
+#git submodule init
+#git submodule update
+## only for hig perf. tests
+##- cd addtional/tests/files; cat Lucy* | tar xz
+
+if [ ! -d $ROOT_PATH/build ]; then mkdir $ROOT_PATH/build; fi
+cd $ROOT_PATH/build
+cmake $CHECKOUT_PATH -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DGRSF_BUILD_GUI=ON -DGRSF_BUILD_MPI=ON -DGRSF_BUILD_NOGUI=ON -DGRSF_BUILD_SIMCONVERTER=ON
+make VERBOSE=1
+
+cd $ROOT_PATH
+
+
+# "BUILD COMPLETE ================================================================"
---
+++ travis/install_dep.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e # exit on errors
+
+cd $ROOT_PATH
+
+
+# Install OpenMPI  =====================================================
+sudo apt-get install openmpi-bin libopenmpi-dev
+
+# Install HDF5  ========================================================
+sudo apt-get install libhdf5-serial-dev hdf5-tools
+
+
+# Install eigen3 =======================================================
+hg clone https://bitbucket.org/eigen/eigen/ ${ROOT_PATH}/eigen3
+cd ${ROOT_PATH}/eigen3 && hg update 3.2
+mkdir ${ROOT_PATH}/eigen3Build
+cd ${ROOT_PATH}/eigen3Build
+cmake ../eigen3 -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
+sudo make VERBOSE=1 install
+
+# Install meta =========================================================
+git clone https://github.com/ericniebler/meta.git ${ROOT_PATH}/meta
+sudo cp -r ${ROOT_PATH}/meta/include/* $INSTALL_PREFIX/include/
+#ls -a /usr/local/include/meta
+
+# Install pugixml  =====================================================
+git clone https://github.com/zeux/pugixml.git ${ROOT_PATH}/pugixml
+perl -pi -e 's/\/\/\s*#define\s*PUGIXML_HAS_LONG_LONG/#define PUGIXML_HAS_LONG_LONG/g' ${ROOT_PATH}/pugixml/src/pugiconfig.hpp 
+mkdir ${ROOT_PATH}/pugixmlBuild
+cd ${ROOT_PATH}/pugixmlBuild
+cmake ../pugixml/scripts/ -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
+sudo make VERBOSE=1 install
+
+# Install boost ========================================================
+ # Install newer boost
+BOOST_DOWNLOAD_URL="http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.bz2/download"
+BOOST_BUILD=${ROOT_PATH}/boostBuild
+mkdir -p ${BOOST_BUILD}
+wget --no-verbose --output-document="${ROOT_PATH}/boost.tar.bz2" "$BOOST_DOWNLOAD_URL"; fi
+if [ "$CXX" = "g++" ]; then export BOOST_TOOLSET=gcc; fi
+if [ "$CXX" = "clang++" ]; then export BOOST_TOOLSET=clang; fi
+cd ${BOOST_BUILD}
+tar jxf "$DOWNLOAD_ROOT/boost.tar.bz2" --strip-components=1 -C "${BOOST_BUILD}"; fi
+./bootstrap.sh --with-toolset="$BOOST_TOOLSET" --with-libraries=system,thread,serialization
+sudo ./b2 toolset="$BOOST_TOOLSET" threading=multi install
+
+
+# Install OGRE 3d
+hg clone http://bitbucket.org/sinbad/ogre -u v1-9 ${ROOT_PATH}/ogre
+mkdir ${ROOT_PATH}/ogreBuild
+cd ${ROOT_PATH}/ogreBuild
+cmake ../ogre -DCMAKE_BUILD_TYPE=Release
+sudo make VERBOSE=1 install
+
+
+
