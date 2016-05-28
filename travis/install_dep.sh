55430f06eb115ce3f0d754af343db08491efd310
Gabriel Nützi <gnuetzi@gmail.com>
2016-05-28 07:59:29 +0200

    Update install_dep.sh

--- travis/install_dep.sh
+++ travis/install_dep.sh
@@ -17,8 +17,8 @@ wget --no-verbose --output-document="${ROOT_PATH}/hdf5.tar.gz" "$HDF5_DOWNLOAD_U
 cd ${HDF5_BUILD}
 tar zxf "${ROOT_PATH}/hdf5.tar.gz" --strip-components=1 -C "${HDF5_BUILD}"
 sudo ./configure --prefix=/usr/local/hdf5 --enable-cxx 
-sudo make
-sudo make install
+sudo make -j${BUILD_CORES}
+sudo make -j${BUILD_CORES} install
 export PATH="$PATH:/usr/local/hdf5"
 
 # Install eigen3 =======================================================
@@ -27,7 +27,7 @@ cd ${ROOT_PATH}/eigen3 && hg update default
 mkdir ${ROOT_PATH}/eigen3Build
 cd ${ROOT_PATH}/eigen3Build
 cmake ../eigen3 -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
-sudo make VERBOSE=1 install
+sudo make -j${BUILD_CORES} VERBOSE=1 install
 ls $INSTALL_PREFIX/lib/cmake
 
 # Install meta =========================================================
@@ -41,7 +41,7 @@ perl -pi -e 's/\/\/\s*#define\s*PUGIXML_HAS_LONG_LONG/#define PUGIXML_HAS_LONG_L
 mkdir ${ROOT_PATH}/pugixmlBuild
 cd ${ROOT_PATH}/pugixmlBuild
 cmake ../pugixml/scripts/ -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
-sudo make VERBOSE=1 install
+sudo make -j${BUILD_CORES} VERBOSE=1 install
 
 # Install boost ========================================================
 # Install newer boost
@@ -52,7 +52,7 @@ wget --no-verbose --output-document="${ROOT_PATH}/boost.tar.bz2" "$BOOST_DOWNLOA
 cd ${BOOST_BUILD}
 tar jxf "${ROOT_PATH}/boost.tar.bz2" --strip-components=1 -C "${BOOST_BUILD}"
 ./bootstrap.sh --with-libraries=system,thread,serialization,filesystem,chrono,atomic,date_time
-sudo ./b2 threading=multi link=shared release install
+sudo ./b2 -j${BUILD_CORES} threading=multi link=shared release install
 
 #alternative
 #sudo apt-get  -y install libboost1.54-all-dev
@@ -63,7 +63,7 @@ git clone https://github.com/assimp/assimp.git assimp
 mkdir -p ${ROOT_PATH}/assimpBuild
 cd ${ROOT_PATH}/assimpBuild
 cmake ../assimp 
-sudo make VERBOSE=1 install
+sudo make -j${BUILD_CORES} VERBOSE=1 install
 
 #alternative
 #sudo apt-get -y install libassimp-dev
@@ -74,14 +74,14 @@ sudo make VERBOSE=1 install
 #cd ${ROOT_PATH}/OIS
 #sudo ./bootstrap
 #sudo ./configure
-#sudo make && sudo make install
+#sudo make -j${BUILD_CORES} && sudo make -j${BUILD_CORES} install
 
 #sudo apt-get -y install libxrandr-dev
 #hg clone http://bitbucket.org/sinbad/ogre -u v1-9 ${ROOT_PATH}/ogre
 #mkdir ${ROOT_PATH}/ogreBuild
 #cd ${ROOT_PATH}/ogreBuild
 #cmake ../ogre -DCMAKE_BUILD_TYPE=Release -DOGRE_BUILD_SAMPLES=OFF -DOGRE_BUILD_TESTS=OFF -DOGRE_BUILD_TOOLS=OFF
-#sudo make VERBOSE=1 install 
+#sudo make -j${BUILD_CORES}VERBOSE=1 install 
 
 # alternative
 sudo apt-get -y install libogre-1.9.0 libogre-1.9-dev libois-1.3.0
