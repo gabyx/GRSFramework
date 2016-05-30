00c8c44011a6c853f16fd913a46ecb3fc13c6123
Gabriel Nützi <gnuetzi@gmail.com>
2016-05-31 01:49:31 +0200

    Update install_dep.sh

--- travis/install_dep.sh
+++ travis/install_dep.sh
@@ -8,83 +8,91 @@ cd $ROOT_PATH
 # Install OpenMPI  =====================================================
 sudo apt-get -y install openmpi-bin libopenmpi-dev
 
-# Install HDF5  ========================================================
-HDF5_DOWNLOAD_URL=https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.15/src/hdf5-1.8.15.tar.gz
-#http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.17.tar.gz
-HDF5_BUILD=${ROOT_PATH}/hdf5Build
-mkdir -p ${HDF5_BUILD}
-wget --no-verbose --output-document="${ROOT_PATH}/hdf5.tar.gz" "$HDF5_DOWNLOAD_URL"
-cd ${HDF5_BUILD}
-tar zxf "${ROOT_PATH}/hdf5.tar.gz" --strip-components=1 -C "${HDF5_BUILD}"
-sudo ./configure --prefix=/usr/local/hdf5 --enable-cxx 
-sudo make -j${BUILD_CORES}
-sudo make -j${BUILD_CORES} install
-export PATH="$PATH:/usr/local/hdf5"
 
-# Install eigen3 =======================================================
-hg clone https://bitbucket.org/eigen/eigen/ ${ROOT_PATH}/eigen3
-cd ${ROOT_PATH}/eigen3 && hg update default
-mkdir ${ROOT_PATH}/eigen3Build
-cd ${ROOT_PATH}/eigen3Build
-cmake ../eigen3 -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
-sudo make -j${BUILD_CORES} install
-ls $INSTALL_PREFIX/lib/cmake
+if [ BUILD_DEPS == "ON"] ; then
+  
+  echo "GRSF Build: Build dependencies! and CACHE them"
 
-# Install meta =========================================================
-git clone https://github.com/ericniebler/meta.git ${ROOT_PATH}/meta
-sudo cp -r ${ROOT_PATH}/meta/include/* $INSTALL_PREFIX/include/
-#ls -a /usr/local/include/meta
+  # Install HDF5  ========================================================
+  HDF5_DOWNLOAD_URL=https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.15/src/hdf5-1.8.15.tar.gz
+  #http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.17.tar.gz
+  HDF5_BUILD=${ROOT_PATH}/hdf5Build
+  mkdir -p ${HDF5_BUILD}
+  wget --no-verbose --output-document="${ROOT_PATH}/hdf5.tar.gz" "$HDF5_DOWNLOAD_URL"
+  cd ${HDF5_BUILD}
+  tar zxf "${ROOT_PATH}/hdf5.tar.gz" --strip-components=1 -C "${HDF5_BUILD}"
+  sudo ./configure --prefix=/usr/local/hdf5 --enable-cxx 
+  sudo make -j${BUILD_CORES}
+  sudo make -j${BUILD_CORES} install
+  export PATH="$PATH:/usr/local/hdf5"
+  
+  # Install eigen3 =======================================================
+  hg clone https://bitbucket.org/eigen/eigen/ ${ROOT_PATH}/eigen3
+  cd ${ROOT_PATH}/eigen3 && hg update default
+  mkdir ${ROOT_PATH}/eigen3Build
+  cd ${ROOT_PATH}/eigen3Build
+  cmake ../eigen3 -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
+  sudo make -j${BUILD_CORES} install
+  ls $INSTALL_PREFIX/lib/cmake
+  
+  # Install meta =========================================================
+  git clone https://github.com/ericniebler/meta.git ${ROOT_PATH}/meta
+  sudo cp -r ${ROOT_PATH}/meta/include/* $INSTALL_PREFIX/include/
+  #ls -a /usr/local/include/meta
+  
+  # Install pugixml  =====================================================
+  git clone https://github.com/zeux/pugixml.git ${ROOT_PATH}/pugixml
+  perl -pi -e 's/\/\/\s*#define\s*PUGIXML_HAS_LONG_LONG/#define PUGIXML_HAS_LONG_LONG/g' ${ROOT_PATH}/pugixml/src/pugiconfig.hpp 
+  mkdir ${ROOT_PATH}/pugixmlBuild
+  cd ${ROOT_PATH}/pugixmlBuild
+  cmake ../pugixml/scripts/ -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
+  sudo make -j${BUILD_CORES} install
+  
+  # Install boost ========================================================
+  # Install newer boost
+  BOOST_DOWNLOAD_URL="http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.bz2/download"
+  BOOST_BUILD=${ROOT_PATH}/boostBuild
+  mkdir -p ${BOOST_BUILD}
+  wget --no-verbose --output-document="${ROOT_PATH}/boost.tar.bz2" "$BOOST_DOWNLOAD_URL"
+  cd ${BOOST_BUILD}
+  tar jxf "${ROOT_PATH}/boost.tar.bz2" --strip-components=1 -C "${BOOST_BUILD}"
+  ./bootstrap.sh --with-libraries=system,thread,serialization,filesystem,chrono,atomic,date_time
+  sudo ./b2 -j${BUILD_CORES} threading=multi link=shared release install
+  
+  #alternative
+  #sudo apt-get  -y install libboost1.54-all-dev
+  
+  # Install Assimp   =====================================================
+  cd ${ROOT_PATH}
+  git clone https://github.com/assimp/assimp.git assimp
+  mkdir -p ${ROOT_PATH}/assimpBuild
+  cd ${ROOT_PATH}/assimpBuild
+  cmake ../assimp 
+  sudo make -j${BUILD_CORES} install
+  
+  #alternative
+  #sudo apt-get -y install libassimp-dev
+  
+  # Install OGRE 3d  =====================================================
+  cd ${ROOT_PATH}
+  git clone https://github.com/wgois/OIS.git OIS
+  cd ${ROOT_PATH}/OIS
+  sudo ./bootstrap
+  sudo ./configure
+  sudo make -j${BUILD_CORES} && sudo make -j${BUILD_CORES} install
+  sudo apt-get -y install libxrandr-dev
+  hg clone http://bitbucket.org/sinbad/ogre -u v1-9 ${ROOT_PATH}/ogre
+  mkdir ${ROOT_PATH}/ogreBuild
+  cd ${ROOT_PATH}/ogreBuild
+  cmake ../ogre -DCMAKE_BUILD_TYPE=Release -DOGRE_BUILD_SAMPLES=OFF -DOGRE_BUILD_TESTS=OFF -DOGRE_BUILD_TOOLS=OFF
+  sudo make -j${BUILD_CORES} install 
+  
+  # alternative
+  #sudo apt-get -y install libogre-1.9.0 libogre-1.9-dev libois-1.3.0 libois-dev
 
-# Install pugixml  =====================================================
-git clone https://github.com/zeux/pugixml.git ${ROOT_PATH}/pugixml
-perl -pi -e 's/\/\/\s*#define\s*PUGIXML_HAS_LONG_LONG/#define PUGIXML_HAS_LONG_LONG/g' ${ROOT_PATH}/pugixml/src/pugiconfig.hpp 
-mkdir ${ROOT_PATH}/pugixmlBuild
-cd ${ROOT_PATH}/pugixmlBuild
-cmake ../pugixml/scripts/ -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
-sudo make -j${BUILD_CORES} install
-
-# Install boost ========================================================
-# Install newer boost
-BOOST_DOWNLOAD_URL="http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.bz2/download"
-BOOST_BUILD=${ROOT_PATH}/boostBuild
-mkdir -p ${BOOST_BUILD}
-wget --no-verbose --output-document="${ROOT_PATH}/boost.tar.bz2" "$BOOST_DOWNLOAD_URL"
-cd ${BOOST_BUILD}
-tar jxf "${ROOT_PATH}/boost.tar.bz2" --strip-components=1 -C "${BOOST_BUILD}"
-./bootstrap.sh --with-libraries=system,thread,serialization,filesystem,chrono,atomic,date_time
-sudo ./b2 -j${BUILD_CORES} threading=multi link=shared release install
-
-#alternative
-#sudo apt-get  -y install libboost1.54-all-dev
-
-# Install Assimp   =====================================================
-cd ${ROOT_PATH}
-git clone https://github.com/assimp/assimp.git assimp
-mkdir -p ${ROOT_PATH}/assimpBuild
-cd ${ROOT_PATH}/assimpBuild
-cmake ../assimp 
-sudo make -j${BUILD_CORES} install
-
-#alternative
-#sudo apt-get -y install libassimp-dev
-
-# Install OGRE 3d  =====================================================
-cd ${ROOT_PATH}
-git clone https://github.com/wgois/OIS.git OIS
-cd ${ROOT_PATH}/OIS
-sudo ./bootstrap
-sudo ./configure
-sudo make -j${BUILD_CORES} && sudo make -j${BUILD_CORES} install
-sudo apt-get -y install libxrandr-dev
-hg clone http://bitbucket.org/sinbad/ogre -u v1-9 ${ROOT_PATH}/ogre
-mkdir ${ROOT_PATH}/ogreBuild
-cd ${ROOT_PATH}/ogreBuild
-cmake ../ogre -DCMAKE_BUILD_TYPE=Release -DOGRE_BUILD_SAMPLES=OFF -DOGRE_BUILD_TESTS=OFF -DOGRE_BUILD_TOOLS=OFF
-sudo make -j${BUILD_CORES} install 
-
-# alternative
-#sudo apt-get -y install libogre-1.9.0 libogre-1.9-dev libois-1.3.0 libois-dev
-
+else
+  echo "GRSF Build: Use cached dependencies"
+fi
 
 # Clone ApproxMVBB  ====================================================
 cd ${ROOT_PATH}
