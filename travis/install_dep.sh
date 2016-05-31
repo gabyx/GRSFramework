b8c2157e95e7e40b47700688016996b93359948d
Gabriel Nützi (UBUNTU) <gnuetzi@gmx.ch>
2016-05-31 20:10:55 +0200

    travis cache local -> cache system copy

--- travis/install_dep.sh
+++ travis/install_dep.sh
@@ -9,9 +9,21 @@ export GRSF_CACHE_SIGNATURE_FILE="$GRSF_CACHE_DIR/GRSF_DEPS_CACHE_SUCCESSFUL"
 # Install OpenMPI  =====================================================
 sudo apt-get -y install openmpi-bin libopenmpi-dev
 
+# Install eigen3 =======================================================
+hg clone https://bitbucket.org/eigen/eigen/ ${ROOT_PATH}/eigen3
+cd ${ROOT_PATH}/eigen3 && hg update default
+mkdir ${ROOT_PATH}/eigen3Build
+cd ${ROOT_PATH}/eigen3Build
+cmake ../eigen3 #-DCMAKE_INSTALL_PREFIX=/usr/local
+sudo make -j${BUILD_CORES} install
+
+# Install meta =========================================================
+git clone https://github.com/ericniebler/meta.git ${ROOT_PATH}/meta
+sudo cp -r ${ROOT_PATH}/meta/include/* /usr/local/include/
+#ls -a /usr/local/include/meta
+
+# large libraries 
 # check if the cache build signature file is here
-
-
 if [  ! -f "$GRSF_CACHE_SIGNATURE_FILE" ] ; then
 
   echo "GRSF Build: Build only dependencies! and CACHE them"
@@ -27,24 +39,9 @@ if [  ! -f "$GRSF_CACHE_SIGNATURE_FILE" ] ; then
   wget --no-verbose --output-document="${ROOT_PATH}/hdf5.tar.gz" "$HDF5_DOWNLOAD_URL"
   cd ${HDF5_BUILD}
   tar zxf "${ROOT_PATH}/hdf5.tar.gz" --strip-components=1 -C "${HDF5_BUILD}"
-  sudo ./configure --prefix=/usr/local/hdf5 --enable-cxx
+  sudo ./configure --prefix=$INSTALL_PREFIX --enable-cxx
   sudo make -j${BUILD_CORES}
   sudo make -j${BUILD_CORES} install
-  export PATH="$PATH:/usr/local/hdf5"
-
-  # Install eigen3 =======================================================
-  hg clone https://bitbucket.org/eigen/eigen/ ${ROOT_PATH}/eigen3
-  cd ${ROOT_PATH}/eigen3 && hg update default
-  mkdir ${ROOT_PATH}/eigen3Build
-  cd ${ROOT_PATH}/eigen3Build
-  cmake ../eigen3 -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
-  sudo make -j${BUILD_CORES} install
-  ls $INSTALL_PREFIX/lib/cmake
-
-  # Install meta =========================================================
-  git clone https://github.com/ericniebler/meta.git ${ROOT_PATH}/meta
-  sudo cp -r ${ROOT_PATH}/meta/include/* $INSTALL_PREFIX/include/
-  #ls -a /usr/local/include/meta
 
   # Install pugixml  =====================================================
   git clone https://github.com/zeux/pugixml.git ${ROOT_PATH}/pugixml
@@ -62,7 +59,7 @@ if [  ! -f "$GRSF_CACHE_SIGNATURE_FILE" ] ; then
   wget --no-verbose --output-document="${ROOT_PATH}/boost.tar.bz2" "$BOOST_DOWNLOAD_URL"
   cd ${BOOST_BUILD}
   tar jxf "${ROOT_PATH}/boost.tar.bz2" --strip-components=1 -C "${BOOST_BUILD}"
-  ./bootstrap.sh --with-libraries=system,thread,serialization,filesystem,chrono,atomic,date_time
+  ./bootstrap.sh --with-libraries=system,thread,serialization,filesystem,chrono,atomic,date_time --prefix=$INSTALL_PREFIX
   sudo ./b2 -j${BUILD_CORES} threading=multi link=shared release install
 
 
@@ -71,7 +68,7 @@ if [  ! -f "$GRSF_CACHE_SIGNATURE_FILE" ] ; then
   git clone https://github.com/assimp/assimp.git assimp
   mkdir -p ${ROOT_PATH}/assimpBuild
   cd ${ROOT_PATH}/assimpBuild
-  cmake ../assimp
+  cmake ../assimp -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
   sudo make -j${BUILD_CORES} install
 
 
@@ -81,25 +78,22 @@ if [  ! -f "$GRSF_CACHE_SIGNATURE_FILE" ] ; then
   git clone https://github.com/wgois/OIS.git OIS
   cd ${ROOT_PATH}/OIS
   sudo ./bootstrap
-  sudo ./configure
+  sudo ./configure --prefix=$INSTALL_PREFIX
   sudo make -j${BUILD_CORES} && sudo make -j${BUILD_CORES} install
   sudo apt-get -y install libxrandr-dev
   hg clone http://bitbucket.org/sinbad/ogre -u v1-9 ${ROOT_PATH}/ogre
   mkdir ${ROOT_PATH}/ogreBuild
   cd ${ROOT_PATH}/ogreBuild
-  cmake ../ogre -DCMAKE_BUILD_TYPE=Release -DOGRE_BUILD_SAMPLES=OFF -DOGRE_BUILD_TESTS=OFF -DOGRE_BUILD_TOOLS=OFF
+  cmake ../ogre -DCMAKE_BUILD_TYPE=Release -DOGRE_BUILD_SAMPLES=OFF -DOGRE_BUILD_TESTS=OFF -DOGRE_BUILD_TOOLS=OFF -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
   sudo make -j${BUILD_CORES} install
 
   # alternative
   #sudo apt-get -y install libogre-1.9.0 libogre-1.9-dev libois-1.3.0 libois-dev
 
   # make a signature file which marks successful cache build to check
-
-
-
   # copy all system stuff in local cache
-  mkdir -p $GRSF_CACHE_DIR
-  sudo cp -r /usr/local/* $GRSF_CACHE_DIR
+  #sudo cp -r /usr/local/* $GRSF_CACHE_DIR
+
   echo "successful" | sudo tee -a $GRSF_CACHE_SIGNATURE_FILE > /dev/null
   echo "content in $GRSF_CACHE_DIR :"
   ls -al $GRSF_CACHE_DIR
@@ -108,7 +102,7 @@ else
   echo "GRSF Build: Use cached dependencies (copy from $GRSF_CACHE_DIR -> /usr/local)"
   echo "content in $GRSF_CACHE_DIR :"
   ls -al $GRSF_CACHE_DIR
-  sudo cp -r $GRSF_CACHE_DIR /usr/local
+  #sudo cp -r $GRSF_CACHE_DIR /usr/local
 
   export BUILD_GRSF="ON"
   export BUILD_DEPS="OFF"
--- travis/install_linux.sh
+++ travis/install_linux.sh
@@ -4,16 +4,16 @@ set -e # exit on error
 
 # "DEPENDECIES ========================================================================"
 
-export INSTALL_PREFIX="/usr/local/"
-export PATH=$INSTALL_PREFIX/bin:$PATH
+
+export INSTALL_PREFIX="$GRSF_CACHE_DIR"
+export PATH="$PATH:$INSTALL_PREFIX/bin"
 
 cd $ROOT_PATH
 
 #install a newer cmake since at this time Travis only has version 2.8.7
-wget --no-check-certificate http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.sh
+wget --no-check-certificate http://www.cmake.org/files/v3.2/cmake-3.2.2-Linux-x86_64.sh > /dev/null
 chmod a+x cmake-3.2.2-Linux-x86_64.sh
-sudo ./cmake-3.2.2-Linux-x86_64.sh --skip-license --prefix=$INSTALL_PREFIX
-
+sudo ./cmake-3.2.2-Linux-x86_64.sh --skip-license --prefix=/usr/local
 
 
 if [ "$CXX" = "g++" ]; then export CXX="g++-${GCC_VERSION}" CC="gcc-${GCC_VERSION}" BOOST_TOOLSET=gcc; fi
