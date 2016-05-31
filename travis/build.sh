1c30ef4d25ff35e2e8e2b58c4ffc3deef9a17deb
Gabriel Nützi <gabrielnuetzi@Gabriels-iMac.local>
2016-05-31 12:41:10 +0200

    travis update

--- travis/build.sh
+++ travis/build.sh
@@ -7,22 +7,24 @@ if [ -z "$BUILD_TYPE" ]; then export BUILD_TYPE=Release; fi
 
 
 
-echo "Build GRSFramework:"
-cd $CHECKOUT_PATH
 
-### init submodules
-#git submodule init
-#git submodule update
-## only for hig perf. tests
-##- cd addtional/tests/files; cat Lucy* | tar xz
-
-if [ ! -d ${ROOT_PATH}/build ]; then mkdir ${ROOT_PATH}/build; fi
-cd ${ROOT_PATH}/build
 
 if [ "$BUILD_GRSF" == "ON" ]; then
 
+  echo "Build GRSFramework..."
+  cd $CHECKOUT_PATH
+
+  ### init submodules
+  #git submodule init
+  #git submodule update
+  ## only for hig perf. tests
+  ##- cd addtional/tests/files; cat Lucy* | tar xz
+
+  if [ ! -d ${ROOT_PATH}/build ]; then mkdir ${ROOT_PATH}/build; fi
+  cd ${ROOT_PATH}/build
+
   echo " ApproxMVBB Repo Dir: ${APPROXMVBB_REPO_DIR}"
-  
+
   cmake $CHECKOUT_PATH -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
                        -DGRSF_BUILD_GUI=$BUILD_GRSF_SIMGUI \
                        -DGRSF_BUILD_MPI=$BUILD_GRSF_SIMMPI \
@@ -32,6 +34,8 @@ if [ "$BUILD_GRSF" == "ON" ]; then
   make VERBOSE=1
   cd $ROOT_PATH
 
+else
+  echo "Do not build GRSFramework!"
 fi
 
 # "BUILD COMPLETE ================================================================"
--- travis/install_dep.sh
+++ travis/install_dep.sh
@@ -8,10 +8,15 @@ cd $ROOT_PATH
 # Install OpenMPI  =====================================================
 sudo apt-get -y install openmpi-bin libopenmpi-dev
 
+# check if the cache build signature file is here
 
-if [ "$BUILD_DEPS" == "ON" ] ; then
-  
-  echo "GRSF Build: Build dependencies! and CACHE them"
+
+if [  ! -f "/usr/local/GRSF_DEPS_CACHE_SUCCESSFUL" ] ; then
+
+  echo "GRSF Build: Build only dependencies! and CACHE them"
+
+  export BUILD_GRSF="OFF"
+  export BUILD_DEPS="ON"
 
   # Install HDF5  ========================================================
   HDF5_DOWNLOAD_URL=https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.15/src/hdf5-1.8.15.tar.gz
@@ -21,11 +26,11 @@ if [ "$BUILD_DEPS" == "ON" ] ; then
   wget --no-verbose --output-document="${ROOT_PATH}/hdf5.tar.gz" "$HDF5_DOWNLOAD_URL"
   cd ${HDF5_BUILD}
   tar zxf "${ROOT_PATH}/hdf5.tar.gz" --strip-components=1 -C "${HDF5_BUILD}"
-  sudo ./configure --prefix=/usr/local/hdf5 --enable-cxx 
+  sudo ./configure --prefix=/usr/local/hdf5 --enable-cxx
   sudo make -j${BUILD_CORES}
   sudo make -j${BUILD_CORES} install
   export PATH="$PATH:/usr/local/hdf5"
-  
+
   # Install eigen3 =======================================================
   hg clone https://bitbucket.org/eigen/eigen/ ${ROOT_PATH}/eigen3
   cd ${ROOT_PATH}/eigen3 && hg update default
@@ -34,45 +39,42 @@ if [ "$BUILD_DEPS" == "ON" ] ; then
   cmake ../eigen3 -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
   sudo make -j${BUILD_CORES} install
   ls $INSTALL_PREFIX/lib/cmake
-  
+
   # Install meta =========================================================
   git clone https://github.com/ericniebler/meta.git ${ROOT_PATH}/meta
   sudo cp -r ${ROOT_PATH}/meta/include/* $INSTALL_PREFIX/include/
   #ls -a /usr/local/include/meta
-  
+
   # Install pugixml  =====================================================
   git clone https://github.com/zeux/pugixml.git ${ROOT_PATH}/pugixml
-  perl -pi -e 's/\/\/\s*#define\s*PUGIXML_HAS_LONG_LONG/#define PUGIXML_HAS_LONG_LONG/g' ${ROOT_PATH}/pugixml/src/pugiconfig.hpp 
+  perl -pi -e 's/\/\/\s*#define\s*PUGIXML_HAS_LONG_LONG/#define PUGIXML_HAS_LONG_LONG/g' ${ROOT_PATH}/pugixml/src/pugiconfig.hpp
   mkdir ${ROOT_PATH}/pugixmlBuild
   cd ${ROOT_PATH}/pugixmlBuild
   cmake ../pugixml/scripts/ -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
   sudo make -j${BUILD_CORES} install
-  
+
   # Install boost ========================================================
   # Install newer boost
-  BOOST_DOWNLOAD_URL="http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.bz2/download"
-  BOOST_BUILD=${ROOT_PATH}/boostBuild
-  mkdir -p ${BOOST_BUILD}
-  wget --no-verbose --output-document="${ROOT_PATH}/boost.tar.bz2" "$BOOST_DOWNLOAD_URL"
-  cd ${BOOST_BUILD}
-  tar jxf "${ROOT_PATH}/boost.tar.bz2" --strip-components=1 -C "${BOOST_BUILD}"
-  ./bootstrap.sh --with-libraries=system,thread,serialization,filesystem,chrono,atomic,date_time
-  sudo ./b2 -j${BUILD_CORES} threading=multi link=shared release install
-  
-  #alternative
-  #sudo apt-get  -y install libboost1.54-all-dev
-  
+  # BOOST_DOWNLOAD_URL="http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.bz2/download"
+  # BOOST_BUILD=${ROOT_PATH}/boostBuild
+  # mkdir -p ${BOOST_BUILD}
+  # wget --no-verbose --output-document="${ROOT_PATH}/boost.tar.bz2" "$BOOST_DOWNLOAD_URL"
+  # cd ${BOOST_BUILD}
+  # tar jxf "${ROOT_PATH}/boost.tar.bz2" --strip-components=1 -C "${BOOST_BUILD}"
+  # ./bootstrap.sh --with-libraries=system,thread,serialization,filesystem,chrono,atomic,date_time
+  # sudo ./b2 -j${BUILD_CORES} threading=multi link=shared release install
+
+
   # Install Assimp   =====================================================
-  cd ${ROOT_PATH}
-  git clone https://github.com/assimp/assimp.git assimp
-  mkdir -p ${ROOT_PATH}/assimpBuild
-  cd ${ROOT_PATH}/assimpBuild
-  cmake ../assimp 
-  sudo make -j${BUILD_CORES} install
-  
-  #alternative
-  #sudo apt-get -y install libassimp-dev
-  
+  # cd ${ROOT_PATH}
+  # git clone https://github.com/assimp/assimp.git assimp
+  # mkdir -p ${ROOT_PATH}/assimpBuild
+  # cd ${ROOT_PATH}/assimpBuild
+  # cmake ../assimp
+  # sudo make -j${BUILD_CORES} install
+
+
+
   # Install OGRE 3d  =====================================================
   cd ${ROOT_PATH}
   git clone https://github.com/wgois/OIS.git OIS
@@ -85,18 +87,32 @@ if [ "$BUILD_DEPS" == "ON" ] ; then
   mkdir ${ROOT_PATH}/ogreBuild
   cd ${ROOT_PATH}/ogreBuild
   cmake ../ogre -DCMAKE_BUILD_TYPE=Release -DOGRE_BUILD_SAMPLES=OFF -DOGRE_BUILD_TESTS=OFF -DOGRE_BUILD_TOOLS=OFF
-  sudo make -j${BUILD_CORES} install 
-  
+  sudo make -j${BUILD_CORES} install
+
   # alternative
   #sudo apt-get -y install libogre-1.9.0 libogre-1.9-dev libois-1.3.0 libois-dev
 
+  # make a signature file which marks successful cache build to check
+
+  echo "content in /usr/local/include :"
+  ls -al /usr/local/include
+
+  sudo echo "successful" > "/usr/local/GRSF_DEPS_CACHE_SUCCESSFUL"
+
 else
-  echo "GRSF Build: Use cached dependencies"
+  echo "GRSF Build: Use cached dependencies:"
+  echo "content in /usr/local/include :"
+  ls -al /usr/local/include
+  export BUILD_GRSF="ON"
+  export BUILD_DEPS="OFF"
+
+  #alternative boost
+  sudo apt-get  -y install libboost1.55-all-dev
+  #alternative assimp
+  sudo apt-get -y install libassimp-dev
 fi
 
 # Clone ApproxMVBB  ====================================================
 cd ${ROOT_PATH}
 git clone https://github.com/gabyx/ApproxMVBB.git ApproxMVBB
 export APPROXMVBB_REPO_DIR=${ROOT_PATH}/ApproxMVBB
-
-
